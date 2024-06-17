553d08a22a4275839fa2cfcf60b90c7bfabcab54
Toka Mohamed <toka.ezzmohamed@gmail.com>
2024-06-17 10:25:49 -0700

    udemy course

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 
@@ -5,86 +6,135 @@ import 'package:flutter/material.dart';
 class LoginScreen extends StatelessWidget {
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
-
+  var FormKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Text(
-                'Login',
-                style: TextStyle(
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 40.0,
-              ),
-              TextFormField(
-                controller: emailController,
-                keyboardType: TextInputType.emailAddress,
-                onFieldSubmitted: (String value){
-                  print(value);
-                },
-                decoration: InputDecoration(
-                  labelText: 'Email address',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.email),
-          
-                ),
-              ),
-              SizedBox(
-                height: 25.0,
-              ),
-              TextFormField(
-                controller: passwordController,
-                decoration: InputDecoration(
-                    labelText: 'Password',
-                  border: OutlineInputBorder(),
-                  prefixIcon: Icon(Icons.lock),
-                  suffixIcon: Icon(Icons.remove_red_eye),
-                ),
-                keyboardType: TextInputType.visiblePassword,
-                obscureText: true,
-              ),
-              SizedBox(
-                height: 25.0,
-              ),
-              Container(
-                width: double.infinity,
-                child: MaterialButton(
-                  onPressed: (){
-                    print(emailController.text);
-                    print(passwordController.text);
-                  },
-                  color: Colors.indigo,
-                  child: Text('Login',
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Form(
+              key: FormKey,
+              child: Column(
                 children: [
                   Text(
-                    'Don\'t have an account?',
+                    'Login',
+                    style: TextStyle(
+                      fontSize: 40.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  TextButton(onPressed: (){},
-                      child: Text(''
-                          'Register Now'))
+                  SizedBox(
+                    height: 40.0,
+                  ),
+                  TextFormField(
+                    controller: emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    onFieldSubmitted: (String value){
+                      print(value);
+                    },
+                    onChanged: (String value){
+                      print(value);
+                    },
+                    validator: (value)
+                  {
+                      if(value!.isEmpty)
+                      {
+                        {return 'This field is required';}
+                      }
+                      return null;
+                  },
+                    decoration: InputDecoration(
+                      labelText: 'Email address',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.email),
+
+                    ),
+                  ),
+                  SizedBox(
+                    height: 25.0,
+                  ),
+                  TextFormField(
+                    controller: passwordController,
+                    validator: (value){
+                      if(value!.isEmpty){
+                        return 'This field is required';
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                        labelText: 'Password',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.lock),
+                      suffixIcon: Icon(Icons.remove_red_eye),
+                    ),
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: true,
+                  ),
+                  SizedBox(
+                    height: 25.0,
+                  ),
+                  Form(
+                    child: Column(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          child: MaterialButton(
+                            onPressed: (){
+                              if(FormKey.currentState!.validate()){
+                                print(emailController.text);
+                                print(passwordController.text);
+                              }
+
+                            },
+                            color: Colors.indigo,
+                            child: Text('Login',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 25.0,
+                        ),
+                        Container(
+                          width: double.infinity,
+                          child: MaterialButton(
+                            onPressed: (){
+                              print(emailController.text);
+                              print(passwordController.text);
+                            },
+                            color: Colors.indigo,
+                            child: Text('Register',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Don\'t have an account?',
+                      ),
+                      TextButton(onPressed: (){},
+                          child: Text(''
+                              'Register Now'))
+                    ],
+                  )
                 ],
-              )
-            ],
+              ),
+            ),
           ),
         ),
       ),
